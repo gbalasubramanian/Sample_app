bd40acab1de5a0915dd7e4b966e35e4d4b0d902c
Gitapriya Balasubramanian <gitapriya@gmail.com>
2011-11-28 15:01:14 +0000

    removed defaultRails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help' 
+  
 
-  get "pages/contact"
+  root :to => 'pages#home'
+
+end
+ #get "pages/home"
+
+  #get "pages/contact"
   
-  get "pages/about"
+  #get "pages/about"
   
-  get "pages/help"
+  #get "pages/help"
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -63,4 +72,4 @@ SampleApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
-end
+
